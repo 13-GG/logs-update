f88613d6ab8929eef0dbc24f379ff316156b304f
kasimov <119078352+dddaannyy@users.noreply.github.com>
2025-10-20 17:00:57 +0300

    Update con_00.php

--- con_00.php
+++ con_00.php
@@ -1,5 +1,5 @@
 <?php
-$current_version = '0.0.5';
+$current_version = '0.0.6';
 
 function checkForUpdates($current_version) {
     $update_file_url = 'https://raw.githubusercontent.com/13-GG/logs-update/main/logs_update.json';
